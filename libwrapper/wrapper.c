cc6de8a0acd82251cc9dc98e539d7fd94b17c026
Emiel Kollof <emiel.kollof@gmail.com>
2011-11-02 16:38:53 +0100

    Fix compilation

--- libwrapper/wrapper.c
+++ libwrapper/wrapper.c
@@ -12,19 +12,89 @@
 
 #define _GNU_SOURCE
 
+#include <sys/param.h>
+#include <errno.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <dlfcn.h>
+#include <string.h>
 
-typedef int (*sys_unlink)(const char *);
+extern int errno;
+
+/* prototypes */
+int fileinpath(const char *, const char *);
+
+
+int (*sys_unlink)(const char *);
+int (*sys_open)(const char *, int, unsigned short);
+int (*sys_open64)(const char *, int, unsigned short);
+FILE *(*sys_fopen)(const char *pathname, const char *mode);
+
+void _init(void) {
+	sys_unlink = dlsym(RTLD_NEXT, "unlink");	
+	sys_open = dlsym(RTLD_NEXT, "open");
+	sys_open64 = dlsym(RTLD_NEXT, "open64");
+	sys_fopen = dlsym(RTLD_NEXT, "fopen");
+}
+
+FILE *fopen(const char *pathname, const char *mode) {
+
+	if (fileinpath(pathname, "SUDO_ALLOWED")) {
+		return (FILE *) sys_fopen(pathname, mode);
+	}
+
+	errno = EPERM;
+	return NULL;
+}
+
+
+int open(const char *pathname, int flags, unsigned short mode) {
+
+	if (fileinpath(pathname, "SUDO_ALLOWED")) {	
+		return sys_open(pathname, flags, mode);
+	} 
+		
+	errno = EPERM;
+	return -1;
+}
+
+int open64(const char *pathname, int flags, unsigned short mode) {
+	return open(pathname, flags, mode);
+}
 
 int unlink(const char *path) { 
-	// Get original unlink address
-	// sys_unlink p = dlsym(RTLD_NEXT, "unlink");	
 
-	fprintf(stderr, "\nWrapped unlink called.\n");
+	printf("\nunlink wrapped\n");
 
-	// Call system "unlink" with our check params
-	// return p(path);
-	return -1;
+	if (fileinpath(path, "SUDO_ALLOWED")) { 
+		return sys_unlink(path);
+	} else { 
+		errno = EPERM;
+		return -1;
+	}
+}
+
+int fileinpath(const char *path, const char *envvar) {
+	/* checks if a file is present in the SUDO_ALLOWED environment variable */
+	char *checkpath = getenv(envvar);
+	const char *ptr = checkpath;
+	char file[256];
+	int n;
+	
+	if (checkpath == NULL) { // Empty, everything goes
+		return 1;
+	}
+
+	// Use sscanf to pick apart a colon delimited path, instead
+	// of things like strtok or strstr.
+	while (sscanf(ptr, "%255[^:]%n", file, &n) == 1) {
+		if (!strcmp(file, path)) {
+			return 1; // Match
+		}
+		ptr += n; // read past number of read chars
+		if (*ptr != ':') break;
+		++ptr; // skip over colon.
+	}
+	return 0;
 }
