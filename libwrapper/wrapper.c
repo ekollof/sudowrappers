3d6465c190134b90f221305310623f6e563ffc24
Emiel Kollof <ekollof@samael.(none)>
2011-11-02 12:37:48 +0100

    Initial commit, start LD_PRELOAD wrapper

---
+++ libwrapper/wrapper.c
@@ -0,0 +1,29 @@
+/* 
+ * This is libwrapper, part of the sudowrapper suite by 
+ * Emiel Kollof <emiel.kollof@nl.ibm.com>.
+ *
+ * This library wraps around some fairly common system and library calls
+ * to ensure correct behavior. 
+ *
+ * The following calls will be wrapped:
+ * unlink, fopen, open, stat, exec
+ *
+ */ 
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <unistd.h>
+#include <dlfcn.h>
+
+typedef int (*sys_unlink)(const char *);
+
+int unlink(const char *path) { 
+	// Get original unlink address
+	sys_unlink p = dlsym(RTLD_NEXT, "unlink");	
+
+	fprintf(stderr, "\nWrapped unlink called.\n");
+
+	// Call system "unlink" with our check params
+	return p(path);
+}
