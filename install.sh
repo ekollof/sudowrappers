27b6e702799ddc4f825aab4b5972777a17b95e50
Emiel Kollof <emiel.kollof@gmail.com>
2011-11-03 14:14:01 +0100

    Add hacky install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+mkdir -p /opt/sudowrappers/
+cp -rp * /opt/sudowrappers/
+cd /opt/sudowrappers/libwrapper
+make
