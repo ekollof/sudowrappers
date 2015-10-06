8fd2ab68afceb040c8b150839626d81220ec3966
Emiel Kollof <emiel.kollof@gmail.com>
2015-10-06 10:59:27 +0200

    Style fixes

--- libwrapper/test.c
+++ libwrapper/test.c
@@ -29,31 +29,31 @@
 #include <stdlib.h>
 
 int main(void) {
-	FILE *fp;
-	char file[] = "foobar";
-	struct stat st;
+    FILE *fp;
+    char file[] = "foobar";
+    struct stat st;
 
-	if ((fp = fopen(file, "a")) == NULL) {
-		perror("fopen");
-		return EXIT_FAILURE;
-	}
-	fprintf(fp, "Hello World\n");
-	fflush(fp);
-	fclose(fp);
+    if ((fp = fopen(file, "a")) == NULL) {
+        perror("fopen");
+        return EXIT_FAILURE;
+    }
+    fprintf(fp, "Hello World\n");
+    fflush(fp);
+    fclose(fp);
 
-	printf("Waiting 2 seconds...\n");
-	sleep(2);
+    printf("Waiting 2 seconds...\n");
+    sleep(2);
 
-	if (unlink(file) < 0) {
-		perror("unlink");
-	}
+    if (unlink(file) < 0) {
+        perror("unlink");
+    }
 
-	if (stat(file, &st) < 0) {
-		printf("File no longer exists.\n");
-		return EXIT_FAILURE;
-	} else {
-		printf("File is still here!\n");
-	}
+    if (stat(file, &st) < 0) {
+        printf("File no longer exists.\n");
+        return EXIT_FAILURE;
+    } else {
+        printf("File is still here!\n");
+    }
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
